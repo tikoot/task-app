ee7ac2fbeb0b2f71a2ba5004b9a90ba63d4c56e4
totarashvili <toarashvili@clp.ge>
2025-03-12 20:01:11 +0400

    Added navigation with button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TaskCreationPage from "./pages/TaskCreationPage";
 
 function App() {
   return (
-    <div>
-      <TaskCreationPage />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/new-task">New Task</Link>
+      </nav>
+      <Routes>
+        <Route path="/new-task" element={<TaskCreationPage />} />
+      </Routes>
+    </Router>
   );
 }
 
