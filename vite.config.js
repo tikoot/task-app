dd57ffe95f45a418dfa9e2ba8d01ca2e2ce78415
totarashvili <toarashvili@clp.ge>
2025-03-21 10:19:01 +0400

    fx

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import tailwindcss from 'tailwindcss' 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
+  plugins: [
+    react(),
+   
+    
   ],
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      input: './index.html', 
+    },
+  },
 })
