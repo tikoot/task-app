1b6eb7bc02af8b35ff087f1f8730c9ebd4cc95e7
totarashvili <toarashvili@clp.ge>
2025-03-21 10:43:00 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-   
-    
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Optional: Adjust path aliases if needed
+    },
+  },
   build: {
     outDir: 'dist', 
     rollupOptions: {
       input: './index.html', 
     },
   },
-})
+});
