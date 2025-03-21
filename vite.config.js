8b474e2c27c426704a80ce9fa5749345b247bef9
totarashvili <toarashvili@clp.ge>
2025-03-21 11:16:12 +0400

    fix tailwind issue

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Optional: Adjust path aliases if needed
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
     rollupOptions: {
-      input: './index.html', 
+      input: './index.html',
     },
   },
-  purge: [
-    './public/**/*.html'
-  ],
 });
